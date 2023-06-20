f91a4980efa8f49d3bcd2cf29bbbfed7f351f2cb
jannat456nur <jannatjenny22@gmail.com>
2023-06-20 23:03:55 +0600

    footer  section addee

--- script.js
+++ script.js
@@ -50,3 +50,26 @@ var swiper = new Swiper('.slide-content', {
     },
   },
 })
+
+//second carousel
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  direction: getDirection(),
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection())
+    },
+  },
+})
+
+function getDirection() {
+  var windowWidth = window.innerWidth
+  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal'
+
+  return direction
+}
